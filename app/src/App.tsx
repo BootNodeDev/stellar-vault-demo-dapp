551808408373c32c234025afefd31efa0c3edb33
Lucho Bonatti <luciano@bootnode.dev>
2026-07-22 11:00:15 -0300

    chore: remove placeholder Contract Explorer (/debug) page and nav link (#9)

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,6 @@
 import { Vault } from "@phosphor-icons/react"
-import { NavLink, Outlet, Route, Routes } from "react-router-dom"
+import { Outlet, Route, Routes } from "react-router-dom"
 import ConnectAccount from "./components/ConnectAccount"
-import Debug from "./pages/Debug"
 import Home from "./pages/Home"
 
 function App() {
@@ -9,8 +8,6 @@ function App() {
 		<Routes>
 			<Route element={<AppLayout />}>
 				<Route path="/" element={<Home />} />
-				<Route path="/debug" element={<Debug />} />
-				<Route path="/debug/:contractName" element={<Debug />} />
 			</Route>
 		</Routes>
 	)
@@ -26,17 +23,6 @@ const AppLayout = () => (
 				<span className="brand-name">Ballast</span>
 				<span className="brand-sub">Reinsurance Vault</span>
 			</div>
-			<nav className="topnav">
-				<NavLink to="/" end className={({ isActive }) => (isActive ? "is-active" : "")}>
-					Vault
-				</NavLink>
-				<NavLink
-					to="/debug"
-					className={({ isActive }) => (isActive ? "is-active" : "")}
-				>
-					Explorer
-				</NavLink>
-			</nav>
 			<div className="topbar-right">
 				<ConnectAccount />
 			</div>
